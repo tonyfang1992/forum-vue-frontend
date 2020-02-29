f208567130116113569533894eb3e7e7be916ed6
tonyfang <q710370@gmail.com>
2020-02-29 16:10:24 +0800

    import moment function

--- src/components/NewestRestaurants.vue
+++ src/components/NewestRestaurants.vue
@@ -9,7 +9,7 @@
           <small>{{ restaurant.Category.name }}</small>
         </h4>
         <p>{{ restaurant.description }}</p>
-        {{ restaurant.createdAt}}
+        {{ restaurant.createdAt | fromNow }}
         <hr />
       </div>
     </div>
@@ -17,7 +17,16 @@
 </template>
 
 <script>
+import moment from "moment";
 export default {
+  filters: {
+    fromNow(datetime) {
+      if (!datetime) {
+        return "-";
+      }
+      return moment(datetime).fromNow();
+    }
+  },
   props: {
     restaurants: {
       type: Array,
